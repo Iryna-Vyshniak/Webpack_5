d3223c19a534b80acba446416ec933566bd924e7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-26 13:09:59 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,6 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
       `...`,
       new CssMinimizerWebpackPlugin(),
       new TerserWebpackPlugin({
